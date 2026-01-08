255a0bef4827925244c05b337ddc787ecb9cf940
端庄光束 <124722631@qq.com>
2026-01-08 11:35:33 +0800

    feat(api): expose /api/insight endpoint for project entry

--- addons/smart_construction_core/__init__.py
+++ addons/smart_construction_core/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 from . import models
+from . import controllers
+from . import services
 from . import wizard
 from .hooks import pre_init_hook, post_init_hook, ensure_core_taxes
