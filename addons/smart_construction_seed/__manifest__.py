20409505a72e1f99bdfdd4d9abe2ada1ead200ea
端庄光束 <124722631@qq.com>
2026-01-14 20:01:47 +0800

    seed: add demo showcase actions and menus

--- addons/smart_construction_seed/__manifest__.py
+++ addons/smart_construction_seed/__manifest__.py
@@ -8,6 +8,7 @@
     "author": "SCE",
     "depends": ["smart_construction_bootstrap", "account", "smart_construction_core"],
     "data": [
+        "data/sc_demo_showcase_actions.xml",
         "data/sc_seed_dictionary_contract.xml",
         "data/sc_seed_partner.xml",
         "data/sc_seed_tax.xml",
