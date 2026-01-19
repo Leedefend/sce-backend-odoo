9acfa0fe5216b488843e6c7bcc9ea230bca085c0
端庄光束 <124722631@qq.com>
2026-01-19 23:05:47 +0800

    chore(p2-progress): relocate progress entry logic

--- addons/smart_construction_core/models/core/__init__.py
+++ addons/smart_construction_core/models/core/__init__.py
@@ -3,6 +3,7 @@ from . import project_core
 from . import purchase_extend
 from . import cost_domain
 from ..support import budget_compat  # 兼容旧 project.budget.line 名称，需在主模型前加载
+from . import progress_entry
 from . import project_project_financial
 from . import project_budget
 from . import boq
