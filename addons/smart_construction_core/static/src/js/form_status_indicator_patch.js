c5c4d630fb2235482b8ed6ed04c7c4083d8e984e
端庄光束 <124722631@qq.com>
2026-01-24 13:06:24 +0800

    feat(ui): label draft save as 暂存 for project.project

--- addons/smart_construction_core/static/src/js/form_status_indicator_patch.js
+++ addons/smart_construction_core/static/src/js/form_status_indicator_patch.js
@@ -6,6 +6,12 @@ import { patch } from "@web/core/utils/patch";
 
 patch(FormStatusIndicator.prototype, {
     get scSaveLabel() {
+        const root = this.props?.model?.root;
+        const resModel = (root && root.resModel) || this.props?.resModel || "";
+        const state = root && root.data ? root.data.lifecycle_state : "";
+        if (resModel === "project.project" && state === "draft") {
+            return _t("暂存");
+        }
         return _t("保存");
     },
     get scDiscardLabel() {
