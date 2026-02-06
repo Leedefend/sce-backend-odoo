4e0d9d890d15de4fa610a2821c285bc380532984
端庄光束 <124722631@qq.com>
2026-02-06 09:41:23 +0800

    feat(scene): add orchestration module and consume config

---
+++ addons/smart_construction_scene/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Smart Construction Scene Orchestration",
+    "version": "17.0.0.1",
+    "summary": "Scene orchestration source for portal shell",
+    "author": "Leedefend",
+    "depends": [
+        "smart_construction_core",
+    ],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
