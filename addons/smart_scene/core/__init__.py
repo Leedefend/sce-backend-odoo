c19d2f550ba1c3c5ed3ffe4e35dbc218a9b42b3b
端庄光束 <124722631@qq.com>
2026-03-14 11:26:57 +0800

    feat(scene): introduce provider registry and industry provider registration

--- addons/smart_scene/core/__init__.py
+++ addons/smart_scene/core/__init__.py
@@ -7,12 +7,26 @@ from .provider_locator import (
 )
 from .scene_contract_builder import build_scene_contract
 from .scene_engine import build_scene_contract_from_specs
+from .scene_provider_registry import (
+    SceneContentProvider,
+    SceneProviderRegistry,
+    build_scene_provider_registry,
+    list_scene_provider_entries,
+    resolve_scene_provider,
+    resolve_scene_provider_path,
+)
 from .scene_resolver import resolve_scene_identity
 
 __all__ = [
     "resolve_workspace_home_provider_path",
     "resolve_project_dashboard_scene_content_path",
     "resolve_scene_registry_content_path",
+    "SceneContentProvider",
+    "SceneProviderRegistry",
+    "build_scene_provider_registry",
+    "resolve_scene_provider",
+    "resolve_scene_provider_path",
+    "list_scene_provider_entries",
     "resolve_scene_identity",
     "build_scene_contract",
     "build_scene_contract_from_specs",
