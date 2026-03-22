841da670d00719b68a5b37615c5bc2ca58dd4925
端庄光束 <124722631@qq.com>
2026-03-22 20:41:37 +0800

    feat(product): add pilot execution precheck

--- addons/smart_construction_core/services/project_execution_builders/__init__.py
+++ addons/smart_construction_core/services/project_execution_builders/__init__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from .project_execution_next_actions_builder import ProjectExecutionNextActionsBuilder
+from .project_execution_pilot_precheck_builder import ProjectExecutionPilotPrecheckBuilder
 from .project_execution_tasks_builder import ProjectExecutionTasksBuilder
 
 
 BUILDERS = (
     ProjectExecutionTasksBuilder,
+    ProjectExecutionPilotPrecheckBuilder,
     ProjectExecutionNextActionsBuilder,
 )
